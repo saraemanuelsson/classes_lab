e60c5e9ca408c0e8a410999c3d30f98b516fa704
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-05-18 15:28:59 +0100

    Refactor to use attr_accessor

--- part_a/part_a.rb
+++ part_a/part_a.rb
@@ -7,22 +7,6 @@ class CodeClanStudent
         @cohort = cohort
     end
 
-    # def name()
-    #     return @name
-    # end
-
-    # def cohort()
-    #     return @cohort
-    # end
-
-    # def change_name(new_name)
-    #     return @name = new_name
-    # end
-
-    # def change_cohort(new_cohort)
-    #     return @cohort = new_cohort
-    # end
-
     def student_talks(quote)
         return quote
     end
