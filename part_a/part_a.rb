ee6d0739e345e1788bcd956c671c45d47328e583
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-05-18 15:14:50 +0100

    Initial commit

---
+++ part_a/part_a.rb
@@ -0,0 +1,27 @@
+class CodeClanStudent
+
+    # attr_reader :name :cohort
+    # attr_writer :name
+
+    def initialize(name, cohort)
+        @name = name
+        @cohort = cohort
+    end
+
+    def name()
+        return @name
+    end
+
+    def cohort()
+        return @cohort
+    end
+
+    def change_name(new_name)
+        return @name = new_name
+    end
+
+    def change_cohort(new_cohort)
+        return @cohort = new_cohort
+    end
+
+end
---
+++ part_a/specs/specs_part_a.rb
@@ -0,0 +1,31 @@
+require('minitest/autorun')
+require('minitest/reporters')
+require_relative('../part_a')
+
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+class TestCodeClanStudent < MiniTest::Test
+
+    def test_student_name
+        student = CodeClanStudent.new("Sara", "E40")
+        assert_equal("Sara", student.name())
+    end
+
+    def test_cohort
+        student = CodeClanStudent.new("Sara", "E40")
+        assert_equal("E40", student.cohort())
+    end
+
+    def test_can_change_name
+        student = CodeClanStudent.new("Sara", "E40")
+        student.change_name("Klaus")
+        assert_equal("Klaus", student.name())
+    end
+
+    def test_can_change_cohort
+        student = CodeClanStudent.new("Sara", "E40")
+        student.change_cohort("E39")
+        assert_equal("E39", student.cohort())
+    end
+
+end
