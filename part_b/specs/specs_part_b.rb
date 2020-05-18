75711c054ba8f80181ed71390e0290548a153b69
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-05-18 17:04:05 +0100

    Refactor setup method

--- part_b/specs/specs_part_b.rb
+++ part_b/specs/specs_part_b.rb
@@ -12,44 +12,42 @@ class TestSportsTeam < MiniTest::Test
         @players = ["Malin Bayard", "Rolf-Goran Bengtsson", "Peder Fredricson", "Helena Jonsson"]
         @coach = "Henrik Ankarcrona"
 
+        @team = SportsTeam.new(@team_name, @players, @coach)
+
     end
 
     def test_team_name
-        team = SportsTeam.new(@team_name, @players, @coach)
-        assert_equal("Sweden", team.team_name())
+        assert_equal("Sweden", @team.team_name())
     end
 
     def test_players
-        team = SportsTeam.new(@team_name, @players, @coach)
-        assert_equal(["Malin Bayard", "Rolf-Goran Bengtsson", "Peder Fredricson", "Helena Jonsson"], team.players())
-        assert_equal(4, team.players().length)
+        assert_equal(["Malin Bayard", "Rolf-Goran Bengtsson", "Peder Fredricson", "Helena Jonsson"], @team.players())
+        assert_equal(4, @team.players().length)
     end
 
     def test_coach 
-        team = SportsTeam.new(@team_name, @players, @coach)
-        assert_equal("Henrik Ankarcrona", team.coach())
+        assert_equal("Henrik Ankarcrona", @team.coach())
     end
 
     def test_change_coach
-        team = SportsTeam.new(@team_name, @players, @coach)
-        team.coach = "Henrik von Eckermann"
-        assert_equal("Henrik von Eckermann", team.coach())
+        @team.coach = "Henrik von Eckermann"
+        assert_equal("Henrik von Eckermann", @team.coach())
     end
 
     def test_add_player
-        team = SportsTeam.new(@team_name, @players, @coach)
-        team.add_player("Helena Lundback")
-        assert_equal(5, team.players().length)
+        @team.add_player("Helena Lundback")
+        assert_equal(5, @team.players().length)
     end
 
     def test_player_is_on_team
-        team = SportsTeam.new(@team_name, @players, @coach)
-        assert_equal(true, team.player_is_on_team("Rolf-Goran Bengtsson"))
+        assert_equal(true, @team.player_is_on_team("Rolf-Goran Bengtsson"))
     end
 
     def test_player_is_not_on_team
-        team = SportsTeam.new(@team_name, @players, @coach)
-        assert_equal(false, team.player_is_on_team("Sara Emanuelsson"))
+        assert_equal(false, @team.player_is_on_team("Sara Emanuelsson"))
     end
+
+    # def test_add_points_property
+    #     assert_equal()
 
 end
